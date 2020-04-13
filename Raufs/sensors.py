1f095260b3dc8fcfc2fc14a1ef79432855a8e057
rienath <infernalion@hotmail.com>
2020-04-14 00:26:36 +0100

    multithreading to monitor co2 (joystick movements)

--- Raufs/sensors.py
+++ Raufs/sensors.py
@@ -1,29 +1,40 @@
 from sense_hat import SenseHat
 from time import sleep
+import threading
+
 sense = SenseHat()
 
-class CO2:
+class CO2(threading.Thread):
 
-        orange = (254, 110, 0)
-        cyan = (0, 255, 255)
+        orange = [254, 110, 0]
+        cyan = [0, 255, 255]
         co2_ppm = 500   # Good CO2 ppm
 
-        def _init_(self):
-                while True:
-                        for event in sense.stick.get_events():
-                                # Check if the joystick was pressed
-                                if event.action == "pressed":
-                
-                                        # Check which direction
-                                        if event.direction == "up":
-                                                sense.show_message("Fire", orange)      # Up arrow
-                                                co2_ppm = 8000         # High CO2 ppm - fire present 
-                                        elif event.direction == "down":
-                                                sense.show_letter("No fire", cyan)      # Down arrow
-                                                co2_ppm = 500
-
+        # Gets current CO2 ppm reading
         def get_reading(self):
-                return co2_ppm
+                return self.co2_ppm
 
-
+        # Since I do not have CO2 sensor, joystick controlls the ppm measure.
+        # If up is pressed, ppm becomes 8000, if down - 500.
+        def monitor_joystick(self):
+                while True:
+                        event = sense.stick.wait_for_event()
+                        
+                        # Check if the joystick was pressed
+                        if event.action == "pressed":
+                                sense.clear()
+                                
+                                # Check which direction
+                                if event.direction == "up":
+                                        self.co2_ppm = 8000                                     # High CO2 ppm - fire present 
+                                        sense.show_message("Fire", text_colour=self.orange)     # Up arrow
+                                elif event.direction == "down":
+                                        self.co2_ppm = 500
+                                        sense.show_message("No fire", text_colour=self.cyan)    # Down arrow
         
+ch = CO2()
+print(2)
+print(ch.get_reading())
+print(ch.get_reading())
+thread = threading.Thread(target=ch.monitor_joystick)
+thread.start()
